b74178ec9b34fb7370b853ecc9b647eab09dd63b
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2021-02-14 20:14:19 +0900

    別の回答方法

--- ABC188/A/B/Main.java
+++ ABC188/A/B/Main.java
@@ -16,15 +16,21 @@ public class Main{
             B[i] = sc.nextInt();
         }
 
-        Integer mult[] = new Integer[N];
-        for(int i = 0; i < N; i++){
-            mult[i] = A[i]*B[i];
-        }
-
+        //別回答
         int ans = 0;
         for(int i = 0; i < N; i++){
-            ans += mult[i];
+            ans += A[i]*B[i];
         }
+
+        // Integer mult[] = new Integer[N];
+        // for(int i = 0; i < N; i++){
+        //     mult[i] = A[i]*B[i];
+        // }
+
+        // int ans = 0;
+        // for(int i = 0; i < N; i++){
+        //     ans += mult[i];
+        // }
 
         if(ans == 0){
             System.out.println("Yes");
