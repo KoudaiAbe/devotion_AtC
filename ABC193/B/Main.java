f91c59aa93049fe4f795220438b028ace725e687
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2021-03-01 22:38:09 +0900

    for文で行列の取得

--- ABC193/B/Main.java
+++ ABC193/B/Main.java
@@ -3,24 +3,21 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int N = sc.nextInt();
-        Integer A[] = new Integer[N];
-        Integer P[] = new Integer[N];
-        Integer X[] = new Integer[N];
+        int cash = -1;
 
         for(int i = 0; i < N; i++){
-            A[i] = sc.nextInt();
-            P[i] = sc.nextInt();
-            X[i] = sc.nextInt();
-        }
+            int A = sc.nextInt();
+            int P = sc.nextInt();
+            int X = sc.nextInt();
 
-        int ans = 0;
-
-        for(int i = 0;i < N;i++){
-            if(X[i] > A[i]){
-
+            if(X > A){
+                if(cash == -1 || cash > P){
+                    cash = P;
+                }
             }
         }
-
+        System.out.println(cash);
     }
 }
