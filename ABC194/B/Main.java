3ce576264ed1389de59518b5f6d02ac8a722c14b
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2021-03-06 23:48:01 +0900

    for２重ループで配列の最小値を取得する

--- ABC194/B/Main.java
+++ ABC194/B/Main.java
@@ -1,63 +1,29 @@
 import java.util.Scanner;
-import java.lang.reflect.Array;
-import java.util.*;
-
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int N = sc.nextInt();
 
-        ArrayList<Integer> A = new ArrayList<>();
-        ArrayList<Integer> B = new ArrayList<>();
-        // Integer A[] = new Integer[N];
-        // Integer B[] = new Integer[N];
+        Integer A[] = new Integer[N];
+        Integer B[] = new Integer[N];
 
         for(int i = 0; i < N; i++){
-            A = sc.next();
-            // B = sc.nextInt();
-
-            // int A_low = A[i];
-            // int B_low = B[i];
-
-            // if(A_low > A[i]){
-            //     A_low = A[i];
-            //     break;
-            // }
-
-            // if(B_low > B[i]){
-            //     B_low = B[i];
-            //     break;
-            // }
-            // System.out.println(A_low);
+            A[i] = sc.nextInt();
+            B[i] = sc.nextInt();
         }
 
-        System.out.println(A);
+        int ans = Integer.MAX_VALUE;
 
-        // for(int i = 0; i < N; i++){
-        //     if(B_low > B[i]){
-        //         B_low = B[i];
-        //     }
-        // }
-
-        // int time = A_low + B_low;
-        // System.out.println(time);
-        // System.out.println(B[0]);
-        // System.out.println(B[1]);
-        // System.out.println(B[2]);
-
-        // System.out.println(A[0]);
-        // System.out.println(A[1]);
-        // System.out.println(A[2]);
-
-        // if(time < B[1]){
-        //     System.out.println(time);
-        // }else{
-        //     if(A[1] > B[1]){
-        //         System.out.println(B[1]);
-        //     }else{
-        //         System.out.println(A[1]);
-        //     }
-        // }
+        for(int i = 0;i < N;i++){
+            for(int j = 0;j < N;j++){
+                if(i == j){
+                    ans = Math.min(ans,A[i] + B[j]);
+                }else{
+                    ans = Math.min(ans,Math.max(A[i],B[j]));
+                }
+            }
+        }
+        System.out.println(ans);
     }
 }
