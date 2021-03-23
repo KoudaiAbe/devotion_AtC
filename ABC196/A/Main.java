aa488f93ba2cf91ba74c84b6fa3afa45b6563626
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2021-03-23 23:19:06 +0900

    A問題書き間違い

--- ABC196/A/Main.java
+++ ABC196/A/Main.java
@@ -1,3 +1,13 @@
-public class Main {
-    
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println(b-c);
+    }
 }
