cca79c851a75fe35a6091d317a2ba8b70a8bc6f0
KoudaiAbe <52918698+KoudaiAbe@users.noreply.github.com>
2021-03-25 23:15:13 +0900

    1日1AC

--- ABC196/B/Main.java
+++ ABC196/B/Main.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
+        String X = sc.nextLine();
+        int result = X.indexOf(".");
 
-        System.out.println(b-c);
+        if(result >= 0){
+            String ans = X.substring(0, result);
+            System.out.println(ans);
+        }else{
+            System.out.println(X);
+        }
     }
 }
